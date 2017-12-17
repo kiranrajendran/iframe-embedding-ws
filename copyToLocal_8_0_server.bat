7fdeac277b056d64ef5e9e8548dd0ede664f71fb
Gianluca Natali <natali.gianluca@gmail.com>
2017-12-17 03:19:18 +0100

    cleanup and changes to the workshop

--- copyToLocal_8_0_server.bat
+++ copyToLocal_8_0_server.bat
@@ -1,13 +1,13 @@
 REM If Server is running, stop it
-for /F "tokens=3 delims=: " %%H in ('sc query "pentahoserver-3" ^| findstr "        STATE"') do (
-  if /I "%%H" EQU "RUNNING" (
+REM for /F "tokens=3 delims=: " %%H in ('sc query "pentahoserver-3" ^| findstr "        STATE"') do (
+REM  if /I "%%H" EQU "RUNNING" (
    REM Put your code you want to execute here
    REM For example, the following line
-   net stop  pentahoserver-3
-  )
-)
+REM   net stop  pentahoserver-3
+REM   )
+REM )
 
-SET src_dir="C:\Users\gnatali\Box Sync\Gianluca Natali\Workspaces\bitbucket\pentaho_cbf2_prj\iframe_embedded\_dockerfiles\patches\tomcat\webapps\pentaho_embedded"
+SET src_dir="%~dp0iframe_embedded\_dockerfiles\patches\tomcat\webapps\pentaho_embedded"
 SET target_dir="C:\Pentaho80\server\pentaho-server\tomcat\webapps\pentaho_embedded"
 @RD /S /Q %target_dir%
 if not exist %target_dir% mkdir %target_dir%
