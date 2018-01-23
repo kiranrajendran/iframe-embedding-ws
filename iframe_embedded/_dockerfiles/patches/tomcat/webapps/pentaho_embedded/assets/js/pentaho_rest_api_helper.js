f202df7e526743a03fc346329fb3d6c4f81396d2
Gianluca Natali <gianluca.natali@hitachivantara.com>
2018-01-23 14:59:33 +0100

    Adding license info

--- iframe_embedded/_dockerfiles/patches/tomcat/webapps/pentaho_embedded/assets/js/pentaho_rest_api_helper.js
+++ iframe_embedded/_dockerfiles/patches/tomcat/webapps/pentaho_embedded/assets/js/pentaho_rest_api_helper.js
@@ -1,7 +1,24 @@
 /*
-Pentaho - Embedding with Iframe Sample
-Author: Gianluca Natali
-*/
+ * Embedding Pentaho using iFrames
+ * 
+ *
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 function PentahoRestApis(serverUrl, pentaho_user, pentaho_pwd) {
     var serverUrl = serverUrl ? serverUrl : "http://localhost:8080/pentaho",
         defaultFrameId = "defaultFrame",
